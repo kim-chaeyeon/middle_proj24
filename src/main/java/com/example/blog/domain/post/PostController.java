2642f483f066036e733b9edcff1faa62d1143da6
kim-chaeyeon <90239727+kim-chaeyeon@users.noreply.github.com>
2024-06-21 17:49:46 +0900

    재병합 (#12)

--- src/main/java/com/example/blog/domain/post/PostController.java
+++ src/main/java/com/example/blog/domain/post/PostController.java
@@ -5,18 +5,11 @@ import com.example.blog.domain.member.entity.Member;
 import com.example.blog.domain.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.Hibernate;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -25,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
+import java.util.List;
 
 @RequestMapping("/post")
 @Controller
@@ -33,63 +27,50 @@ public class PostController {
     private final PostService postService;
     private final MemberService memberService;
 
-//    @GetMapping("/list")
-//    public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
-//                       @RequestParam(value = "kw", defaultValue = "") String kw, Principal principal) {
-//        Member member = memberService.getCurrentMember();
-//
-//        if (member != null && member.getUsername().equals("admin")) {
-//            Page<Post> paging = postService.getList(page, kw); // admin일 경우 모든 게시물 가져오기
-//            model.addAttribute("paging", paging);
-//            model.addAttribute("loggedInUser", member);
-//        } else {
-//            String region = member.getRegion();
-//            Page<Post> paging = postService.getList(page, kw, region); // 일반 사용자일 경우 지역별 게시물 가져오기
-//            model.addAttribute("paging", paging);
-//            model.addAttribute("loggedInUser", member);
-//        }
-//
-//        return "post_list";
-//    }
-
     @GetMapping("/list")
-    public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
-                       @RequestParam(value = "kw", defaultValue = "") String kw, Principal principal) {
-
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || !authentication.isAuthenticated() || authentication instanceof AnonymousAuthenticationToken) {
-            return "redirect:/member/login"; // 로그인이 되어 있지 않은 경우 로그인 페이지로 리다이렉트
-        }
-
+    public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page, @RequestParam(value = "kw", defaultValue = "")String kw) {
+        Page<Post> paging = this.postService.getList(page, kw);
+        model.addAttribute("paging", paging);
 
         Member member = memberService.getCurrentMember();
-
-        if (member != null && member.getUsername().equals("admin")) {
-            Page<Post> paging = postService.getList(page, kw); // admin일 경우 모든 게시물 가져오기
-            model.addAttribute("paging", paging);
-            model.addAttribute("loggedInUser", member);
-        } else {
-            String region = member.getRegion();
-            Page<Post> paging = postService.getList(page, kw, region); // 일반 사용자일 경우 지역별 게시물 가져오기
-            model.addAttribute("paging", paging);
-            model.addAttribute("loggedInUser", member);
-        }
-
+        model.addAttribute("loggedInUser", member);
         return "post_list";
     }
 
+    @GetMapping("/memberPosts/{nickname}")
+    public String getPostsByAuthorNickname(@PathVariable String nickname, Model model, @RequestParam(defaultValue = "0") int page) {
+        List<Post> memberPosts = postService.getPostsByAuthorNickname(nickname);
+        model.addAttribute("userPosts", memberPosts); // "userPosts"로 변경
+        model.addAttribute("nickname", nickname); // "member"로 변경
+        return "member_posts";
+    }
+
+//
+//    @GetMapping("/list/{nickname}")
+//    public String listByUser(Model model, @PathVariable("nickname") String nickname,
+//                             @RequestParam(value = "page", defaultValue = "0") int page,
+//                             @RequestParam(value = "kw", defaultValue = "") String kw) {
+//        Page<Post> paging = this.postService.getPostsByUsername(nickname, page, kw);
+//        model.addAttribute("paging", paging);
+//        model.addAttribute("nickname", nickname);
+//
+//        return "post_list"; // 리디렉션 제거, 직접 뷰를 반환하도록 수정
+//    }
 
 
     @GetMapping("/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id, CommentForm commentForm) {
         Post p = this.postService.getPost(id);
+
         model.addAttribute("post", p);
+
         return "post_detail";
     }
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/create")
     public String create(PostForm postForm) {
+
         return "post_form";
     }
 
@@ -101,15 +82,18 @@ public class PostController {
             @RequestParam("thumbnail") MultipartFile thumbnail,
             Principal principal) {
 
+        // 유효성 검사 로직 추가 가능
         if (title.isEmpty() || content.isEmpty() || thumbnail.isEmpty()) {
+            // 에러 처리 로직
             return "post_form";
         }
 
-        Member member = memberService.getCurrentMember();
-        Post post = postService.create(title, content, thumbnail, member, member.getRegion());
+        Member member = this.memberService.getCurrentMember();
+        Post p = this.postService.create(title, content, thumbnail, member);
 
         return "redirect:/post/list";
     }
+
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/modify/{id}")
     public String postModify(@Valid PostForm postForm, BindingResult bindingResult,
@@ -119,9 +103,8 @@ public class PostController {
         }
 
         Post post = postService.getPost(id);
-        Member member = memberService.getCurrentMember();
 
-        if (!post.getAuthor().getUsername().equals(member.getUsername())) {
+        if (!post.getAuthor().getUsername().equals(principal.getName())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
         }
 
@@ -134,9 +117,8 @@ public class PostController {
     @GetMapping("/modify/{id}")
     public String postModify(PostForm postForm, @PathVariable("id") Integer id, Principal principal) {
         Post post = postService.getPost(id);
-        Member member = memberService.getCurrentMember();
 
-        if (!post.getAuthor().getUsername().equals(member.getUsername())) {
+        if (!post.getAuthor().getUsername().equals(principal.getName())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
         }
 
@@ -148,43 +130,24 @@ public class PostController {
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/delete/{id}")
     public String postDelete(Principal principal, @PathVariable("id") Integer id) {
-        Post post = postService.getPost(id);
-        Member member = memberService.getCurrentMember();
+        Post post = this.postService.getPost(id);
 
-        // 관리자 권한 추가
-        if (!post.getAuthor().getUsername().equals(member.getUsername())
-                && !memberService.isAdmin(member)
-                && !member.getUsername().equals("admin")) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        if (!post.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
         }
 
         postService.delete(post);
 
-        return "redirect:/";
+        return "redirect:/post/list";
     }
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/vote/{id}")
     public String postVote(Principal principal, @PathVariable("id") Integer id) {
         Post post = this.postService.getPost(id);
-        Member member = memberService.getCurrentMember();
-
+        Member member = this.memberService.getCurrentMember();
         this.postService.vote(post, member);
 
         return "redirect:/post/detail/%s".formatted(id);
-    }
-
-    @GetMapping("/myPost")
-    @PreAuthorize("isAuthenticated()")
-    public String myPosts(Model model, Principal principal, @RequestParam(value = "page", defaultValue = "0") int page) {
-        Member member = memberService.getCurrentMember();
-        Pageable pageable = PageRequest.of(page, 10);  // 한 페이지에 10개의 게시물 표시
-
-        Page<Post> myPosts = postService.getPostsByAuthor(member, pageable);
-
-        model.addAttribute("myPosts", myPosts);
-        model.addAttribute("loggedInUser", member);
-
-        return "myPost";
     }
 }
