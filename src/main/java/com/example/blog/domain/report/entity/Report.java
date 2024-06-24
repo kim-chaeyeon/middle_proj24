27398c5085cc8f091feaa1de9d21ae79939c5585
kim-chaeyeon <90239727+kim-chaeyeon@users.noreply.github.com>
2024-06-24 15:32:19 +0900

    병합 (#13)

--- src/main/java/com/example/blog/domain/report/entity/Report.java
+++ src/main/java/com/example/blog/domain/report/entity/Report.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 @Entity
 public class Report {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
